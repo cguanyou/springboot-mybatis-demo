af8b47655c6b551982ac5a3512cdbeef9d8fdcb5
chen <chen@192.168.254.1>
2018-06-07 15:45:21 +0800

    sssss

--- src/main/java/com/syswin/comntoon/controller/DemoController.java
+++ src/main/java/com/syswin/comntoon/controller/DemoController.java
@@ -1,18 +1,14 @@
 package com.syswin.comntoon.controller;
 
 import java.util.HashMap;
-import java.util.Map;
 
 import javax.annotation.Resource;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.syswin.comntoon.model.CustomCard;
 import com.syswin.comntoon.service.impl.CustomCardService;
 @Controller
 @ComponentScan({"com.syswin.comntoon.service"})
@@ -22,6 +18,7 @@ public class DemoController {
     private CustomCardService customCardService;
     @RequestMapping("/coffeebank")
     public String helloHtml(HashMap<String, Object> map) {
+    	System.out.println("ssssss");
         map.put("coffee", "欢迎进入咖啡银行启动页面"+"jenkinstest！！！！！！");
         return "/comntoon/coffeebankdemo/coffeebankindex";
     }
