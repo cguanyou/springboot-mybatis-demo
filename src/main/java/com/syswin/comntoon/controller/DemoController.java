fbe61c6ff2da92ece56f86eb6cd8248bf0bbe9d5
chen <chen@192.168.254.1>
2018-06-07 20:25:36 +0800

    22222

--- src/main/java/com/syswin/comntoon/controller/DemoController.java
+++ src/main/java/com/syswin/comntoon/controller/DemoController.java
@@ -19,7 +19,7 @@ public class DemoController {
     @RequestMapping("/coffeebank")
     public String helloHtml(HashMap<String, Object> map) {
     	System.out.println("ssssss");
-        map.put("coffee", "欢迎进入咖啡银行启动页面"+"jenkins~~~~~~~~!!!!!!");
+        map.put("coffee", "欢迎进入咖啡银行启动页面"+"jenkins");
         return "/comntoon/coffeebankdemo/coffeebankindex";
     }
     
